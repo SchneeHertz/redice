aa1053d1b453863f384cf7b2ca1a29de6e134be9
SchneeHertz <snowhzhang@gmail.com>
2021-12-03 08:30:36 +0800

    update icon

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,21 @@
 import path from 'path'
 import fs from 'fs/promises'
+import XLSX from 'xlsx'
+
+// ;(async ()=>{
+//   for (let folder of await fs.readdir('./assets', {withFileTypes: true})) {
+//     if (folder.isDirectory()) {
+//       let fileList = await fs.readdir(path.resolve('./assets', folder.name))
+//       fileList = fileList.filter(p=>p != 'list.json')
+//       await fs.writeFile(path.resolve('./assets', folder.name, './list.json'), JSON.stringify(fileList, null, '  '))
+//     }
+//   }
+// })()
 
 ;(async ()=>{
-  for (let folder of await fs.readdir('./assets', {withFileTypes: true})) {
-    if (folder.isDirectory()) {
-      let fileList = await fs.readdir(path.resolve('./assets', folder.name))
-      fileList = fileList.filter(p=>p != 'list.json')
-      await fs.writeFile(path.resolve('./assets', folder.name, './list.json'), JSON.stringify(fileList, null, '  '))
-    }
-  }
+  let workbook = XLSX.readFile("../data.xlsx")
+  let mutationList = XLSX.utils.sheet_to_json(workbook.Sheets["Sheet1"])
+  await fs.writeFile('./assets/mutationList.json', JSON.stringify(mutationList, null, "  "))
+  let eventList = XLSX.utils.sheet_to_json(workbook.Sheets['Sheet2'])
+  await fs.writeFile('./assets/eventList.json', JSON.stringify(eventList, null, "  "))
 })()
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
           <el-card id="mutation-card">
             <template #header>
               <span>进化</span>
-              <el-switch v-model="mutationSwitch" class="mutation-switch"/>
+              <el-switch v-model="mutationSwitch" class="mutation-switch" :disabled="disableMutationSwitch"/>
             </template>
             <el-row class="icon-warp">
               <el-col :span="6" v-for="mutation in mutationShowList1" :key="mutation.key">
@@ -20,7 +20,7 @@
                     ref="mutation"
                     v-if="mutation.show"
                   />
-                  <img src="img/mutation_icons/emptyRB.png" class="mutation-score" v-else/>
+                  <img :src="`img/mutation_icons/+${mutation.value}.png`" class="mutation-score" v-else/>
                 </transition>
               </el-col>
             </el-row>
@@ -35,7 +35,7 @@
                     ref="mutation"
                     v-if="mutation.show"
                   />
-                  <img src="img/mutation_icons/emptyRB.png" class="mutation-score" v-else/>
+                  <img :src="`img/mutation_icons/+${mutation.value}.png`" class="mutation-score" v-else/>
                 </transition>
               </el-col>
             </el-row>
@@ -52,19 +52,26 @@
                 <Roll
                   eventNow="empty.png"
                   :event_icons="event_icons"
-                  :eventAfter="eventAfter"
+                  :eventAfter="eventShow.icon"
                   pathString="img/event_icons/"
                   ref="eventroll"
                 />
               </el-col>
               <el-col :span="8">
-                <img src="img/number_icons/bn8.png" class="event-value"/>
+                <Roll
+                  eventNow="empty.png"
+                  :event_icons="number_icons"
+                  :eventAfter="`bn${eventShow.value}.png`"
+                  pathString="img/number_icons/"
+                  ref="eventrollvalue"
+                />
+                <!-- <img :src="`img/number_icons/bn${eventShow.value}.png`" class="event-value"/> -->
               </el-col>
               <el-col :span="8">
                 <Roll
                   eventNow="empty.png"
                   :event_icons="number_icons"
-                  eventAfter="bn7.png"
+                  :eventAfter="`bn${eventRolled}.png`"
                   pathString="img/number_icons/"
                   ref="eventresultroll"
                 />
@@ -85,10 +92,13 @@
             <template #header>
               <span>状态</span>
             </template>
+            <el-descriptions class="margin-top"  :column="3" size="small">
+              <el-descriptions-item v-for="(value, key) in status" :key="key" :label="key">{{value}}</el-descriptions-item>
+            </el-descriptions>
           </el-card>
         </el-col>
       </el-row>
-      <p>地球时间 {{formatTime(timer)}} 年</p>
+      <div class="timer">地球时间 {{formatTime(timer)}} 年</div>
     </el-card>
   </div>
 </template>
@@ -99,6 +109,7 @@ import event_icons from '@/assets/event_icons.json'
 import mutation_icons from '@/assets/mutation_icons.json'
 import number_icons from '@/assets/number_icons.json'
 import mutationList from '@/assets/mutationList.json'
+import eventList from '@/assets/eventList.json'
 import _ from 'lodash'
 import Roll from '@/components/roll.vue'
 
@@ -111,37 +122,41 @@ export default {
     return {
       timer: 0,
       framerate: 60,
-      showEventImage: true,
-      eventSwitch: true,
-      event_icons: event_icons,
-      mutation_icons: mutation_icons,
+      letters:['a', 'ch', 'd', 'e', 'f', 'g', 'ie', 'ii', 'iu', 'k', 'l', 'm', 'n', 'oo', 'oz', 'p', 'ps', 'r', 's', 't', 'th', 'v', 'x', 'z'],
       number_icons: number_icons,
       mutationList: mutationList,
+      mutation_icons: mutation_icons,
+      mutationSwitch: false,
+      disableMutationSwitch: false,
+      existMutationList: [],
       mutationShowList: [
-        {icon: 'bgv.png', show: true, key: 0.1},
-        {icon: 'cgm.png', show: true, key: 0.2},
-        {icon: 'bgn.png', show: true, key: 0.3},
-        {icon: 'cgie.png', show: true, key: 0.4},
-        {icon: 'cgv.png', show: true, key: 0.5},
-        {icon: 'cgii.png', show: true, key: 0.6},
-        {icon: 'bgth.png', show: true, key: 0.7},
-        {icon: 'rbgk.png', show: true, key: 0.8},
+        {icon: 'bgv.png', show: true, key: 0.1, value: 10},
+        {icon: 'cgm.png', show: true, key: 0.2, value: 10},
+        {icon: 'bgn.png', show: true, key: 0.3, value: 10},
+        {icon: 'cgie.png', show: true, key: 0.4, value: 10},
+        {icon: 'cgv.png', show: true, key: 0.5, value: 10},
+        {icon: 'cgii.png', show: true, key: 0.6, value: 10},
+        {icon: 'bgth.png', show: true, key: 0.7, value: 10},
+        {icon: 'rbgk.png', show: true, key: 0.8, value: 10},
       ],
-      mutationSwitch: true,
-      eventNow: _.sample(event_icons),
-      eventAfter: _.sample(event_icons),
+      eventList: eventList,
+      event_icons: event_icons,
+      eventSwitch: false,
+      eventShow: {
+        icon: 'rbgk.png',
+        value: 9
+      },
+      eventRolled: 0,
       status: {
-        mutationPoint: 0,
+        mutationPoint: 11400,
         mutationRate: 1,
-        quantity: 1,
-        weight: 1,
-        lifetime: 1,
-        phenotypicTrait: [],
+        eventPoint: 0,
+        biomass: 100,
+        reproductiverate: 0.1,
+        lifetime: 0.00023,
         intelligence: 1,
-        natality: 0.1,
-        mortality: 0.1,
         ecologicalNichePoint: 1,
-        feedRange: 0.5
+        phenotypicTrait: [],
       }
     }
   },
@@ -151,27 +166,56 @@ export default {
     },
     mutationShowList2 () {
       return this.mutationShowList.slice(4, 8)
-    }
+    },
   },
   watch: {
-    eventSwitch: function () {
-      this.$refs.eventroll.roundEvent()
-      setTimeout(()=>this.$refs.eventresultroll.roundEvent(), 3000)
-    },
-    mutationSwitch: function () {
-      this.mutationShowList.map(e=>{
-        this.$set(e, 'show', true)
-        this.$set(e, 'key', Math.random())
-      })
-      for (let i=0; i<8; i++) {
-        setTimeout(()=>{
-          this.$refs.mutation[i].roundEvent()
-          if (i==7) setTimeout(()=>{
-            this.$set(this.mutationShowList[4], 'show', false)
-            this.$set(this.mutationShowList[6], 'show', false)
-          }, 250*i+2750)
-        }, 250*i)
-      }
+    status: {
+      handler: function (val, oldVal) {
+        if (val.mutationPoint >= 12000 && !this.mutationSwitch) {
+          this.mutationSwitch = true
+          this.$set(val, 'mutationPoint', val.mutationPoint - 1200)
+          this.mutationShowList = _.fill(Array(8), 0).map(()=>({
+            show: true,
+            key: _.random(0, 99999),
+            ...this.pickIcon()
+          }))
+          for (let i=0; i<8; i++) {
+            if (i !== 7) {
+              setTimeout(()=>{
+                this.$refs.mutation[i].roundEvent()
+              }, 250*i)
+            } else {
+              setTimeout(()=>{
+                this.$refs.mutation[i].roundEvent()
+                setTimeout(()=>{
+                  this.convertMutationPoint()
+                  setTimeout(()=>{this.mutationSwitch = false}, 1000)
+                }, 5000)
+              }, 250*i)
+            }            
+          }
+        }
+        if (val.eventPoint >= 1000 && !this.eventSwitch) {
+          this.eventSwitch = true
+          this.$set(val, 'eventPoint', val.eventPoint - 1000)
+          this.eventShow = {
+            icon: this.pickIcon().icon,
+            value: _.random(0, 9)
+          }
+          setTimeout(()=>{
+            this.$refs.eventroll.roundEvent()
+            this.$refs.eventrollvalue.roundEvent()
+            this.eventRolled = _.random(0, 9)
+            setTimeout(()=>{
+              this.$refs.eventresultroll.roundEvent()
+              setTimeout(()=>{
+                this.eventSwitch = false
+              }, 4500)
+            }, 4500)
+          }, 1000)
+        }
+      },  
+      deep: true
     }
   },
   mounted () {
@@ -179,6 +223,42 @@ export default {
     this.timeFlow()
   },
   methods: {
+    calculateBiomass () {
+      this.$set(this.status, 'biomass', this.status.biomass * (1+this.status.reproductiverate))
+    },
+    pickIcon () {
+      let seed = _.random(1,100)
+      let color, type, letter, value
+      type = 'g'
+      if (seed > 98) {
+        color = 'rb'
+        letter = _.sample(this.letters)
+        value = 500
+      } else if (seed > 80) {
+        color = 'c'
+        letter = _.sample(this.letters)
+        value = 100
+      } else {
+        color = 'b'
+        letter = _.sample(this.letters)
+        value = 10
+      }
+      return {
+        icon: `${color}${type}${letter}.png`,
+        value: value
+      }
+    },
+    convertMutationPoint () {
+      _.forIn(this.mutationShowList, mutation=>{
+        if (this.existMutationList.includes(mutation.icon)) {
+          this.$set(mutation, 'show', false)
+          this.$set(this.status, 'mutationPoint', this.status.mutationPoint+mutation.value)
+        } else {
+          this.existMutationList.push(mutation.icon)
+        }
+        this.$set(this.status, 'eventPoint', this.status.eventPoint + 50)
+      })
+    },
     formatTime (timer) {
       return new Intl.NumberFormat().format(timer)
     },
@@ -200,7 +280,11 @@ export default {
     timeFlow () {
       let count = 0
       let tick = () => {
-        if (++count % this.framerate == 0) this.timer += 60
+        if (++count % this.framerate == 0) {
+          this.timer += 60
+          this.$set(this.status, 'mutationPoint', this.status.mutationPoint + 100)
+          this.calculateBiomass()
+        }
         requestAnimationFrame(tick)
       }
       requestAnimationFrame(tick)
@@ -211,13 +295,16 @@ export default {
 
 <style lang="stylus" scoped>
 #content
+  max-width 1280px
+  min-width 480px
+  margin auto
   .el-card
     text-align left
     margin-bottom 10px
   #mutation-card, #event-card, #logger-card
     height 334px
   #status-card
-    height 350px
+    height 240px
   .event-switch, .mutation-switch
     float right 
   .event-value
