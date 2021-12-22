c2e466be3e57f69445d495a00d88ef5f1f8b99a9
SchneeHertz <snowhzhang@gmail.com>
2021-12-22 15:17:24 +0800

    test build

--- docs/stock.worker.js
+++ docs/stock.worker.js
@@ -1,10 +1,12 @@
 importScripts('lodash.min.js')
 let second = 0
 
+let time = 0
 let leverage = 1
 let money = 0
 let price = 1
 let stock = 0
+let hands = 0
 let marketHistory = [{
   open: 1,
   close: 1,
@@ -15,9 +17,12 @@ let marketHistory = [{
 
 const updateMarkerPrice = () => {
   let seed = _.random(-98,100)
+  stock = _.round(stock + hands * price * (seed/10000) * leverage, 2)
   price = _.round(price*(1+seed/10000), 4)
-  stock = _.round(stock*(1+seed/10000*leverage), 2)
-  if (stock < 0) stock = 0
+  if (stock <= 0) {
+    hands = 0
+    stock = 0
+  }
   second += 1
   if (second >= 60) {
     marketHistory.push({
@@ -38,11 +43,13 @@ const updateMarkerPrice = () => {
 }
 
 const stockBuy = (trade) => {
+  hands += _.round(trade / price)
   stock += trade
   money -= trade
 }
 
 const stockSell = (trade) => {
+  hands -= _.round(trade / price)
   stock -= trade
   money += trade
 }
@@ -62,12 +69,15 @@ onmessage = (event)=>{
 }
 
 setInterval(()=>{
+  time += 1
   money += 1
   updateMarkerPrice()
   postMessage({
     money,
     price,
     stock,
-    marketHistory
+    hands,
+    marketHistory,
+    revenueRatio: _.round((money+stock) / time - 1, 4)
   })
 }, 1000)
